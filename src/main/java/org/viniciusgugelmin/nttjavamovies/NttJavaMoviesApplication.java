751a055aba13a4690680223daa7ef1a3e495f9d6
Vinicius <vinigugelmin@gmail.com>
2024-03-26 22:11:31 -0300

    Project init

---
+++ src/main/java/org/viniciusgugelmin/nttjavamovies/NttJavaMoviesApplication.java
@@ -0,0 +1,13 @@
+package org.viniciusgugelmin.nttjavamovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NttJavaMoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NttJavaMoviesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/viniciusgugelmin/nttjavamovies/NttJavaMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.viniciusgugelmin.nttjavamovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttJavaMoviesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
