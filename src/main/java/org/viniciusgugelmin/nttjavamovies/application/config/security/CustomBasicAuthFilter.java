a7d3033765ea84e4ff70ffb1ce31e49c862c12ab
Vinicius <vinigugelmin@gmail.com>
2024-04-05 23:27:52 -0300

    feat: spring security added

---
+++ src/main/java/org/viniciusgugelmin/nttjavamovies/application/config/security/CustomBasicAuthFilter.java
@@ -0,0 +1,17 @@
+package org.viniciusgugelmin.nttjavamovies.application.config.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class CustomBasicAuthFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/org/viniciusgugelmin/nttjavamovies/application/config/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package org.viniciusgugelmin.nttjavamovies.application.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new CustomBasicAuthFilter(), BasicAuthenticationFilter.class)
+                .authorizeRequests(requests -> requests.anyRequest().permitAll())
+                /*.authorizeRequests(requests -> requests
+                        .requestMatchers(HttpMethod.POST, "/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/**").hasRole("USER")
+                        .anyRequest().authenticated()
+                )*/
+                .build();
+    }
+}
